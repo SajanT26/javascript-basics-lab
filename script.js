0371bf3034744ac5dca867d29be504e8ac9b7fe4
SajanT26 <n01715615@humber.ca>
2024-09-21 01:28:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let studentName = "Sajan Thavarasa";
+console.log(studentName); 
+let studentAge = 20;
+console.log(studentAge); 
+let isEnrolled = true;
+console.log(isEnrolled); 
+
+let lastname = "Thavarasa";
+console.log(lastname); 
+
+let kilometre = 79;
+console.log(kilometre); 
+
+let human = true;
+console.log(human); 
+
+let brands = ['Nike', 'Addidas', 'Underarmour'];
+console.log(brands); 
+
+const person = {
+   name: "Sajan",
+   age: 20,
+   birthyear: 2004
+ }
+console.log(person)
+
+
+console.log(10 + 7); 
+console.log(7 - 5); 
+console.log(8 * 7); 
+console.log(90 / 9); 
+let y = 3
+let x = 7
+console.log(x > y); 
+console.log(x < y); 
+console.log (x === y); 
+console.log (x !== y); 
+let b = true;
+let a = false;
+console.log(b && a); 
+console.log(b || a); 
+console.log(!b); 
